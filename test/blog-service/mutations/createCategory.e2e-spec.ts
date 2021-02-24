0d1e7a392662a864eb1b723ce9ab320cd4637bff
Tomasz Trębacz <trebacztomasz@outlook.com>
2021-02-24 23:14:28 +0100

    test: add e2e tests for createCategory mutation

---
+++ test/blog-service/mutations/createCategory.e2e-spec.ts
@@ -0,0 +1,158 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../../src/app.module';
+import { adminToken, confirmedUserToken } from 'test/utils';
+import * as faker from 'faker';
+
+describe('createCategory [mutation](e2e)', () => {
+  let app: INestApplication;
+  let token: String;
+  let categoryName: String;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    token = await adminToken(request, app);
+    categoryName = await faker.lorem.words(2);
+  });
+
+  const mutation =
+    'mutation($input: CreateCategoryInput) { \
+        createCategory(createCategoryInput: $input) { \
+          id \
+          name \
+        } \
+      }';
+
+  describe('if user has permissions and name is unique & between 3 and 20 characters', () => {
+    it('should create and return category', () => {
+      return request(app.getHttpServer())
+        .post('/graphql')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          query: mutation,
+          variables: {
+            input: {
+              name: categoryName,
+            },
+          },
+        })
+        .expect(200)
+        .expect(({ body }) => {
+          expect(body.data.createCategory).toMatchObject({
+            id: expect.any(Number),
+            name: categoryName,
+          });
+        });
+    });
+  });
+
+  describe('otherwise', () => {
+    describe('if user is not logged', () => {
+      it('should throw the error', () => {
+        return request(app.getHttpServer())
+          .post('/graphql')
+          .send({
+            query: mutation,
+            variables: {
+              input: {
+                name: categoryName,
+              },
+            },
+          })
+          .expect(200)
+          .expect(res => {
+            expect(res.body.errors[0].message).toEqual(`No auth token`);
+          });
+      });
+    });
+    describe('if user is not logged as admin or root', () => {
+      it('should throw the error', async () => {
+        const userToken = await confirmedUserToken(request, app);
+
+        return await request(app.getHttpServer())
+          .post('/graphql')
+          .set('Authorization', `Bearer ${userToken}`)
+          .send({
+            query: mutation,
+            variables: {
+              input: {
+                name: categoryName,
+              },
+            },
+          })
+          .expect(200)
+          .expect(res => {
+            expect(res.body.errors[0].message).toEqual(`Wrong role!`);
+          });
+      });
+    });
+    describe('if name is not unique', () => {
+      it('should throw the error', () => {
+        return request(app.getHttpServer())
+          .post('/graphql')
+          .set('Authorization', `Bearer ${token}`)
+          .send({
+            query: mutation,
+            variables: {
+              input: {
+                name: categoryName,
+              },
+            },
+          })
+          .expect(200)
+          .expect(res => {
+            expect(res.body.errors[0].message).toEqual(`This name is taken!`);
+          });
+      });
+    });
+    describe('if name is too short', () => {
+      it('should throw the error', () => {
+        return request(app.getHttpServer())
+          .post('/graphql')
+          .set('Authorization', `Bearer ${token}`)
+          .send({
+            query: mutation,
+            variables: {
+              input: {
+                name: 'la',
+              },
+            },
+          })
+          .expect(200)
+          .expect(res => {
+            expect(res.body.errors[0].message).toEqual(
+              `name must be longer than or equal to 3 characters`,
+            );
+          });
+      });
+    });
+    describe('if name is too long', () => {
+      it('should throw the error', () => {
+        return request(app.getHttpServer())
+          .post('/graphql')
+          .set('Authorization', `Bearer ${token}`)
+          .send({
+            query: mutation,
+            variables: {
+              input: {
+                name: 'toolongnametoolongname',
+              },
+            },
+          })
+          .expect(200)
+          .expect(res => {
+            expect(res.body.errors[0].message).toEqual(
+              `name must be shorter than or equal to 20 characters`,
+            );
+          });
+      });
+    });
+  });
+});
